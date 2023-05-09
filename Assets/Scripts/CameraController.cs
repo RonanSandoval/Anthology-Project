43f1b1dacfc6616050f629e82918e8a85a9e74e9
Ronan Sandoval <ronanchristophersandoval1@gmail.com>
2023-05-09 01:54:19 -0600

    lighting, basic dash

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,17 +7,17 @@ public class CameraController : MonoBehaviour
     public GameObject playerObject;
     public float cameraSpeed = 5f;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private float cameraSin = 0f;
+    public float cameraSinFrequency;
+    public float cameraSinAmplitude;
 
     // Update is called once per frame
     void Update()
     {   
-        Vector3 targetPosition = new Vector3(playerObject.transform.position.x, playerObject.transform.position.y + 2, playerObject.transform.position.z - 7);
+        cameraSin += Time.deltaTime * cameraSinFrequency;
+        float yOffset = Mathf.Sin(cameraSin) * cameraSinAmplitude;
+
+        Vector3 targetPosition = new Vector3(playerObject.transform.position.x, playerObject.transform.position.y + 2 + yOffset, playerObject.transform.position.z - 7);
         transform.position = Vector3.Lerp(transform.position, targetPosition, cameraSpeed * Time.deltaTime);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,11 +10,30 @@ public class Player : MonoBehaviour
     public float dashCooldownLength = 0;
     public float dashCooldown = 0;
     public bool isDashing = false;
+    private bool dashed = false;
+
+    public float dashPower;
+
+    private SpriteRenderer sr;
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = this.GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update() {
+        // dash
+        if (Input.GetKeyDown("space") && dashCooldown <= 0) {
+            dashed = true;
+            Debug.Log("dash");
+            dashCooldown = dashCooldownLength;
+        }
+
+        if (dashCooldown > 0) {
+            dashCooldown -= Time.deltaTime;
+        }
     }
 
 
@@ -23,12 +42,20 @@ public class Player : MonoBehaviour
         //Store user input as a movement vector
         Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
 
-        //Apply the movement vector to the current position, which is
-        //multiplied by deltaTime and speed for a smooth MovePosition
-        rb.MovePosition(transform.position + input * Time.deltaTime * speed);
+        //rb.MovePosition(transform.position + input * Time.deltaTime * speed);
+        rb.AddForce(input * speed);
 
-        if (dashCooldown >= 0) {
-            dashCooldown -= Time.deltaTime;
+        if (dashed) {
+            rb.velocity = new Vector3((input * dashPower).x, rb.velocity.y, (input * dashPower).z);
+            isDashing = true;
+            sr.color = new Color(1,0,1,1);
+            dashed = false;
+        }
+
+        if (isDashing && rb.velocity.magnitude < 15) {
+            isDashing = false;
+            Debug.Log("Done Dash");
+            sr.color = new Color(1,1,1,1);
         }
     }
 }
