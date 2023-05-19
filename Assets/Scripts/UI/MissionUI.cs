7429588ba8b6c1b0bdc3631a0dd729676fbc6ecd
Ronan Sandoval <ronanchristophersandoval1@gmail.com>
2023-05-19 02:22:56 -0600

    Quest update flash

--- Assets/Scripts/UI/MissionUI.cs
+++ Assets/Scripts/UI/MissionUI.cs
@@ -8,17 +8,33 @@ public class MissionUI : MonoBehaviour
     [SerializeField] Text missionTitle;
     [SerializeField] Text missionProgress;
 
+    Image image;
+
     // Start is called before the first frame update
     void Start()
     {
         GameTaskManager.Instance.onTaskUpdate.AddListener(taskUpdate);
         GameTaskManager.Instance.onTaskComplete.AddListener(taskComplete);
+
+        image = GetComponent<Image>();
     }
 
     public void taskUpdate() {
         GameTaskManager.Task currentTask = GameTaskManager.Instance.getCurrentTask();
         missionTitle.text = currentTask.getDescription();
         missionProgress.text = currentTask.getProgressText();
+        StartCoroutine(updateCoroutine());
+    }
+
+    IEnumerator updateCoroutine() {
+        image.color = new Color(1f,1f, 1f, 1f);
+
+        while (image.color.b - 0.5f > 0.01) {
+            yield return null;
+            image.color = Color.Lerp(image.color, new Color(0f,0f, 0.5f, 0.6f), Time.deltaTime * 3f);
+        }
+
+        image.color = new Color(0f,0f, 0.5f, 0.6f);
     }
 
     public void taskComplete() {
@@ -28,6 +44,6 @@ public class MissionUI : MonoBehaviour
     }
 
     public void taskStart() {
-        
+
     }
 }
